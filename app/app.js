d3bd87d7484928d000506fbd7e0e25ee82177300
eLeontev <joonior199@gmail.com>
2018-07-17 22:23:30 +0300

    insert possible battle field

--- app/app.js
+++ app/app.js
@@ -1,3 +1,30 @@
 import {BaseCell} from './cells';
 
-console.log(new BaseCell());
+let playfield = document.getElementById('playfield_canvas');
+let ctx = playfield.getContext('2d');
+let cellsCount = 20;
+
+let width = playfield.getAttribute('width');
+let cellSize = width / cellsCount;
+
+let randomInteger = () => Math.round(-0.5 + Math.random() * (10));
+
+let colours = [
+    'green', 'red', 'black', 'yellow', 'orange', 'beige',
+    'blue', 'aliceblue', 'antiquewhite', 'aquamarine'
+];
+
+let setCells = ({x, y, fillStyle}) => {
+    ctx.fillStyle = colours[randomInteger()];
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+}
+
+let battleField = new Array(cellsCount).fill()
+    .map((e, x) => new Array(cellsCount).fill()
+        .map((e, y) => ({
+            x, 
+            y, 
+            fillStyle: colours[randomInteger()]
+        })));
+
+battleField.forEach((row) => row.forEach(setCells));
